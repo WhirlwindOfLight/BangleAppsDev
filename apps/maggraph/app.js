0f796ce7ea92396451a1123431bfdec20e28f455
WhirlwindOfLight <144183077+WhirlwindOfLight@users.noreply.github.com>
2025-02-09 16:56:02 -0700

    Added maggraph as quick fork of accelgraph

---
+++ apps/maggraph/app.js
@@ -0,0 +1,126 @@
+Bangle.loadWidgets();
+g.clear(1);
+Bangle.drawWidgets();
+var R = Bangle.appRect;
+var Layout = require("Layout");
+var abs = Math.abs;
+
+var x = 0;
+var last;
+var max = {c: 1, mag:2};
+var displayCounter = 0;
+
+var graphArea = 0.60; // Only use 60% of the drawing area
+var wiperWidth = 20;
+var graphFrozen = false;
+var refreshMult = 3; // How many graph updates before a display value update
+
+var color = {
+  mag: g.theme.fg,
+  x: g.theme.dark ? "#f0f" : "#f00",
+  y: g.theme.dark ? "#ff0" : "#0f0",
+  z: g.theme.dark ? "#0ff" : "#00f"
+};
+
+let txt = (myStr, id) => ({ id: id, type: "txt", font: "6x8", label: myStr, fillx: 1});
+let vField = (id) => (txt("-#####,", id));
+var layout = new Layout({
+  type: "v",
+  c: [
+    { type: "v", c: [
+      txt("Values:"),
+      { type: "h", c: [
+        vField("magVal"),
+        txt("("),
+        vField("xVal"),
+        vField("yVal"),
+        vField("zVal"),
+        txt(")")
+      ]},
+    ], filly: 1},
+    { height: R.h * graphArea + 2},
+    { type: "v", c: [
+      txt("Maximums:"),
+      { type: "h", c: [
+        vField("magMax"),
+        txt("("),
+        vField("xMax"),
+        vField("yMax"),
+        vField("zMax"),
+        txt(")")
+      ]},
+    ], filly: 1}
+  ]
+}, {lazy: true});
+layout.update();
+
+function displayValue(myBoxId, value, addComma) {
+  addComma = addComma|false;
+  layout[myBoxId].label = String(value).padStart(6).substring(0, 6);
+  if (addComma) layout[myBoxId].label += ",";
+}
+function getY(v) {
+  return E.clip(-v, -1, 1)*(graphArea)*(R.h/2) + (R.y+R.y2)/2;
+}
+function drawWiper(v) {
+  let v2 = v-1+wiperWidth;
+  g.clearRect(v,getY(-1),v2,getY(1))
+    .setColor("#777")
+    .drawLine(v,getY(0),v2,getY(0))
+    .drawLine(v,getY(1),v2,getY(1))
+    .drawLine(v,getY(-1),v2,getY(-1));
+}
+function drawLineSegment(vec, compName) {
+  g.setColor(color[compName]).drawLine(
+    x-1, getY(last[compName]/max[compName]),
+    x, getY(vec[compName]/max[compName]));
+}
+
+Bangle.setUI({mode: "updown"},
+  (dir) => {
+    if (!dir) { // tap or button press
+      graphFrozen=!graphFrozen;
+    } else { // swipe
+      max = {c: 1, mag:2};
+    }
+  }
+);
+Bangle.setCompassPower(1, "maggraph");
+Bangle.on('mag', n => {
+  n.mag = Math.sqrt(n.x*n.x+n.y*n.y+n.z*n.z);
+  if (graphFrozen) return;
+  // Set Maximums
+  if (abs(n.x) > max.c) {max.c = abs(n.x);}
+  if (abs(n.y) > max.c) {max.c = abs(n.y);}
+  if (abs(n.z) > max.c) {max.c = abs(n.z);}
+  if (n.mag > max.mag) {max.mag = n.mag;}
+  max.x = max.y = max.z = max.c;
+  // Display Maximums
+  displayValue("magMax", max.mag);
+  displayValue("xMax", max.x, true);
+  displayValue("yMax", max.y, true);
+  displayValue("zMax", max.z);
+  // Display Values
+  displayValue("magVal", n.mag);
+  displayValue("xVal", n.x, true);
+  displayValue("yVal", n.y, true);
+  displayValue("zVal", n.z);
+  // Render Data
+  if (!displayCounter) layout.render();
+  // Render Graph
+  g.reset();
+  if (x+wiperWidth<=g.getWidth()) {drawWiper(x);}
+  if (last) {
+    drawLineSegment(n, "mag");
+    drawLineSegment(n, "x");
+    drawLineSegment(n, "y");
+    drawLineSegment(n, "z");
+  }
+  // Increment Loop Variables
+  last = n;x++;
+  displayCounter = (displayCounter + 1) % refreshMult;
+  if (x>=g.getWidth()) {
+    x = 1;
+    drawWiper(0);
+  }
+});
