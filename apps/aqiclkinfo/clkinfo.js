578f12076984b477d510ff3ddb53773152346299
WhirlwindOfLight <144183077+WhirlwindOfLight@users.noreply.github.com>
2025-03-15 18:53:12 -0600

    aqiclkinfo: Patched null reference errors and potential memory leaks

--- apps/aqiclkinfo/clkinfo.js
+++ apps/aqiclkinfo/clkinfo.js
@@ -10,18 +10,24 @@
             require("Storage").read("Ogden_AQI.txt")
         }),
         show: ()=>{
+          if (timeout !== undefined) clearTimeout(timeout);
           timeout = setTimeout(()=>{
             ci.items[0].emit("redraw");
+            if (interval !== undefined) clearInterval(interval);
             interval = setInterval(()=>{
               ci.items[0].emit("redraw");
             }, 6e4);
           }, 6e4 - (Date.now() % 6e4));
         },
         hide: ()=>{
-          clearTimeout(timeout);
-          timeout = undefined;
-          clearInterval(interval);
-          interval = undefined;
+          if (timeout !== undefined) {
+            clearTimeout(timeout);
+            timeout = undefined;
+          }
+          if (interval !== undefined) {
+            clearInterval(interval);
+            interval = undefined;
+          }
         }
       }
     ]
