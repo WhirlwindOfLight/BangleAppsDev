9df008538584b5058fe277e12dc629a9f6c9f84e
WhirlwindOfLight <144183077+WhirlwindOfLight@users.noreply.github.com>
2026-02-16 19:38:15 -0700

    Got started on trying to define Typescript types to improve IDE linting

---
+++ modules-ext/modules.d.ts
@@ -0,0 +1,8 @@
+// locally declared modules
+declare module "sched" {export = Sched}
+declare module "ClockFace" {export = ClockFace_.ClockFace}
+declare module "clock_info" {export = ClockInfo}
+declare module "Layout" {export = Layout.Layout}
+declare module "power_usage" {export = PowerUsageModule}
+declare module "exstats" {export = ExStats}
+declare module "time_utils" {export = TimeUtils}
